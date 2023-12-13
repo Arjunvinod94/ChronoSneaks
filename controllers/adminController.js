671cb700a214ebb39f031d74a1d65ac0fb759fb5
Arjun Vinod <arjunvinod9494@gmail.com>
2023-12-14 02:11:31 +0530

    feat: admin-dashboard, user-listing, add, edit and delete

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,5 +1,55 @@
 const User = require('../models/userModel')
 const bcrypt = require('bcrypt')
+const randomstring = require('randomstring')
+const config = require('../config/config')
+const nodemailer = require('nodemailer')
+
+//password hashing
+const securePassword = async(password)=>{
+    try {
+
+        const passwordHash = await bcrypt.hash(password, 10)
+        return passwordHash
+
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+//for send mail
+
+const addUserMail = async(name, email, password, user_id)=>{
+
+    try {
+        
+        const transporter = nodemailer.createTransport({
+            host:'smtp.gmail.com',
+            port:587,
+            secure:false,
+            requireTLS:true,
+            auth:{
+                user:'chronosneaks@gmail.com',
+                pass:'fnpn eqbf ikiv spaw'
+            }
+        })
+        const mailOptions = {
+            from: 'chronosneaks@gmail.com',
+            to: email,
+            subject: 'For Verifying the mail',
+            html: '<p>Hi '+name+', here is your link for the verification of ChronoSneaks <a href = "http://localhost:3000/verify?id='+user_id+'"> Verify </a></p> <br><br> <b>Email:</b>'+email+'<br><b>Password:</b>'+password+''
+        }
+        transporter.sendMail(mailOptions, (error,info)=>{
+            if(error){
+                console.log(error);
+            } else {
+                console.log("Email has been sent",info.response);
+            }
+        })
+
+    } catch (error) {
+        console.log(error.message);
+    }
+}
 
 //admin login
 const loadLogin = async(req,res)=>{
@@ -71,7 +121,93 @@ const logout = async(req,res)=>{
 // admin dashboard for user management
 const adminDashboard = async(req,res)=>{
     try {
-        res.render('dashboard')
+        const usersData = await User.find({is_admin:0})
+        res.render('dashboard',{users:usersData})
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+//admin add new user(get)
+const newUserLoad = async(req,res)=>{
+    try {
+        res.render('new-user')
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+//(post)
+const addUser = async(req,res)=>{
+    try {
+
+        const name = req.body.name
+        const email = req.body.email
+        const mno = req.body.mno
+        const password = randomstring.generate(8)
+
+        const spassword = await securePassword(password)
+
+        const user = new User({
+            name:name,
+            email:email,
+            mobile:mno,
+            password:spassword,
+            is_admin:0
+        })
+
+        const userData = await user.save()
+
+        if(userData){
+            addUserMail(name, email, password, userData._id)
+            res.redirect('/admin/dashboard')
+
+        }else{
+            res.render('new-user',{message:'Something Wrong'})
+        }
+        
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+//admin edit user(get)
+const editUserLoad = async(req,res)=>{
+    try {
+        const id = req.query.id
+        const userData = await User.findById({_id:id})
+        if(userData){
+            res.render('edit-user',{user:userData})
+        }else{
+            res.redirect('/admin/dashboard')
+        }
+
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+//admin edit user (post)
+const updateUsers = async(req,res)=>{
+    try {
+
+        const userData = await User.findByIdAndUpdate({ _id:req.body.id },{$set:{ name:req.body.name, email:req.body.email, mobile:req.body.mno, is_verified:req.body.verify }})
+        
+        res.redirect('/admin/dashboard')
+
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+// delete user
+const deleteUser = async(req,res)=>{
+    try {
+        
+        const id = req.query.id
+        await User.deleteOne({ _id:id })
+        res.redirect('/admin/dashboard')
+
     } catch (error) {
         console.log(error.message);
     }
@@ -83,5 +219,10 @@ module.exports = {
     verifyLogin,
     loadDashboard,
     logout,
-    adminDashboard
+    adminDashboard,
+    newUserLoad,
+    addUser,
+    editUserLoad,
+    updateUsers,
+    deleteUser
 }
--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -12,6 +12,9 @@ admin_route.use(bodyParser.urlencoded({extended:true}))
 admin_route.set('view engine','ejs')
 admin_route.set('views','./views/admin')
 
+const multer = require('multer')
+const path = require('path')
+
 const auth = require('../middleware/adminAuth')
 
 const adminController = require('../controllers/adminController')
@@ -27,6 +30,17 @@ admin_route.get('/logout',auth.isLogin,adminController.logout)
 
 //admin dashboard
 admin_route.get('/dashboard',auth.isLogin,adminController.adminDashboard)
+
+//admin add new user
+admin_route.get('/new-user',auth.isLogin,adminController.newUserLoad)
+admin_route.post('/new-user',adminController.addUser)
+
+//admin edit user
+admin_route.get('/edit-user',auth.isLogin,adminController.editUserLoad)
+admin_route.post('/edit-user',adminController.updateUsers)
+
+//delete user
+admin_route.get('/delete-user',adminController.deleteUser)
 
 //always loaad admin when wrong route is entered (used last)
 admin_route.get('*',(req,res)=>{
